7d6e966d815625b174dbcde9c07615b1f82c033d
kin8564 <kin8564@rit.edu>
2024-11-08 10:03:39 -0500

    Fleshed out ADC code, need to finish calculations

--- Lab8-ADC.c
+++ Lab8-ADC.c
@@ -27,82 +27,58 @@
 BOOLEAN Timer1RunningFlag = FALSE;
 BOOLEAN Timer2RunningFlag = FALSE;
 
-unsigned long MillisecondCounter = 0;
-
-
+double pulses = 0;
 
 // Interrupt Service Routine for Timer32-1
 void Timer32_1_ISR(void)
 {
-	char BPMBuf[32];
+	char temp[64];
 	unsigned int analogIn = 0;
-	double BPM = 0.0;
-	double Vout = 0.0;	
-	double VoutThreshhold = 1000.0; 				//Idk how this will need to be measured
-	static BOOLEAN Timer1RunningFlag = FALSE;
-	unsigned long MillisecondCounter = 0; 			//not float, need to change?
+	double VoutThreshhold = 6600.0; 				//measured lowest height of the lowest frequency input
 	
 	analogIn = ADC_In();
 	
-//	//Print decimal
-//	uart0_put("\n\rDecimal Value: ");
-//	sprintf(temp, "%i", analogIn);
-//	uart0_put(temp); 
-
-//	//Print HEX
-//	uart0_put("\n\rHex Value: 0x");
-//	sprintf(temp, "%X", analogIn);
-//	uart0_put(temp);
-//	uart0_put("\r\n");	
-
-	
-	//Print BPM
-	Vout = (2.5 * analogIn) / (pow(2.0, 14.0)-1.0);  //What this do?
-	//Vout = analogIn;
+	//Print decimal
+	uart0_put("\n\rDecimal Value: ");
+	sprintf(temp, "%i", analogIn);
+	uart0_put(temp);
 
 	//voltage went past threshhold, indicating a pulse
-	if ((Vout>VoutThreshhold) && !timerStart){
-		//start timer
-		Timer1RunningFlag = true;
-		while(Timer1RunningFlag){
-
-			MillisecondCounter++
-
-			//end timer at next pulse
-			if((Vout>VoutThreshhold) && timerStart){
-			Timer1RunningFlag = false;
-
-			//divide 1min by time measured (make sure units match)
-			BPM = 60000.0 / MillisecondCounter ;
-
-			//reset timer
-			MillisecondCounter = 0;
-			}
-
-		}
+	if ((analogIn>VoutThreshhold)){
+		//pulse has occured
+		pulses++;
 	}
-
+	
+}
+// Interrupt Service Routine for Timer32-1
+void Timer32_2_ISR(void)
+{
+	char temp[64];
+	double BPM = 0.0;
+	
+	//Should be even, so combine rising and falling edges
+	pulses = pulses / 2;
+	
+	//TODO
+	//divide pulses by time unit
+	BPM = pulses / 60000.0 ;
+	
+	//TODO
 	//print BPM to PuTTY
 	//tempIn = Vout * 10;		//10mV to V this needed?
 	uart0_put("\n\rBPM: ");
-	sprintf(BPMBuf, "%f", BPM);
-	uart0_put(BPMBuf);
-	for (i = 0; i < 32; i++) {
-				BPMBuf[i] = NULL;
-		}
+	sprintf(temp, "%f", BPM);
+	uart0_put(temp);
+	
 }
-
-
-
 int main(void)
 {
-	//char temp[64];
-	//unsigned int analogIn = 0;
 	//initializations
 	uart0_init();
 	uart0_put("\r\nLab8 ADC demo\r\n");
 
-	Timer32_1_Init(&Timer32_1_ISR, CalcPeriodFromFrequency(2), T32DIV1); // initialize Timer A32-1;
+	Timer32_1_Init(&Timer32_1_ISR, CalcPeriodFromFrequency(10), T32DIV1); // initialize Timer A32-1;
+	Timer32_2_Init(&Timer32_2_ISR, CalcPeriodFromFrequency(2), T32DIV1);		// initialize Timer A32-2
 
 	LED1_Init();
 	LED2_Init();
